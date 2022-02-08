c4571f6d3e81d5567b4aaa427654ead156d712fc
tcayllahua <tacayllahua@indracompany.com>
2022-02-07 20:12:21 -0500

    create componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { ChangePasswordComponent } from './changepassword/change-password.component';
+import { SendEmailComponent } from './changepassword/send-email.component';
+import { LoginGuard } from './guards/login.guard';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './auth/login.component';
+import { RegistroComponent } from './auth/registro.component';
+import { ProdGuardService } from './guards/prod-guard.service';
+
+
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'registro', component: RegistroComponent, canActivate: [LoginGuard] },
+  { path: 'sendemail', component: SendEmailComponent, canActivate: [LoginGuard] },
+  { path: 'change-password/:tokenPassword', component: ChangePasswordComponent, canActivate: [LoginGuard] },
+  { path: 'lista', component: ListaProductoComponent, canActivate: [ProdGuardService], data: { expectedRol: ['admin', 'user'] } },
+  { path: 'detalle/:id', component: DetalleProductoComponent, canActivate: [ProdGuardService], data: { expectedRol: ['admin', 'user'] } },
+  { path: 'nuevo', component: NuevoProductoComponent, canActivate: [ProdGuardService], data: { expectedRol: ['admin'] } },
+  { path: 'editar/:id', component: EditarProductoComponent, canActivate: [ProdGuardService], data: { expectedRol: ['admin'] } },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
